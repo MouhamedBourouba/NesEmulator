f07853fc2230f26e50bb651ba02a8568d1e34913
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2024-09-20 14:55:35 +0100

    basic ram impl

--- main.c
+++ main.c
@@ -1,7 +1,39 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include "cpu.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define ARRAY_LEN(arr) sizeof(arr) / sizeof(arr[0])
+
+BYTE ram[1024 * 10];
+BYTE read(WORD value){
+  if(value > ARRAY_LEN(ram)) {
+    printf("Invalid index read returned 0");
+    return 0;
+  }
+  if (value == 0xFFFA) {
+    return 0x10;
+  }
+  if (value == 0xFFFB) {
+    return 0x00;
+  }
+  return ram[value];
+}
+void write(WORD address, BYTE value){
+  if(address > ARRAY_LEN(ram)) {
+    printf("Invalid index write");
+    return;
+  }
+  ram[address] = value;
+  return;
+}
 
 int main(void) {
   printf("we have nes emulator in home\n");
+  memset(ram, 0, sizeof(ram));
+
+  Cpu* cpu = Mos6502_create(read, write);
+  Mos6502_tick(cpu, 100);
+
   return EXIT_SUCCESS;
 }
